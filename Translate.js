0a2550fdcaa744808c561eef46e1e4a29718ff69
kalyandindu <pavan.dindu@gmail.com>
2021-09-29 00:32:37 +0530

    Final commit

--- Translate.js
+++ Translate.js
@@ -1,15 +1,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
 var outputText = document.querySelector("#txt-output")
-var serverUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl= "https://api.funtranslations.com/translate/minion.json";
 
 function appendInputTextToUrl(text){
     return serverUrl + "?text=" + text;
 }
 
-btnTranslate.addEventListener("click", function clickFunction(){
-    fetch(appendInputTextToUrl(inputText)).then(Response => Response.json()).then(json => console.log(json));
-})
+function errorHandler(error){   
+    console.log("Error:" + error);
+    alert("Server is not responding, Please try after sometime");
+}   
+
+function clickFunction(){
+    fetch(appendInputTextToUrl(inputText.value)).then(response => response.json()).then(json => {outputText.innerText=json.contents.translated}).catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickFunction);
 
 
 
