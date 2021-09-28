e5e216b85012ce9a9f30e59b0d7920db55723ccf
kalyandindu <pavan.dindu@gmail.com>
2021-09-28 18:51:57 +0530

    Feat: Output

--- Translate.js
+++ Translate.js
@@ -1,5 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
+var outputText = document.querySelector("#txt-output")
 btnTranslate.addEventListener("click", function clickFunction(){
-    console.log(inputText.value);
+    outputText.innerHTML=inputText.value;
 })
