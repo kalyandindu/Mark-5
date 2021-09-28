ee32da517d60653dbdf9e48cd8bfb1fa1d2ecb7c
kalyandindu <pavan.dindu@gmail.com>
2021-09-28 23:43:17 +0530

    Wire fetch Call

--- Translate.js
+++ Translate.js
@@ -1,6 +1,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
 var outputText = document.querySelector("#txt-output")
+var serverUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function appendInputTextToUrl(text){
+    return serverUrl + "?text=" + text;
+}
+
 btnTranslate.addEventListener("click", function clickFunction(){
-    outputText.innerHTML=inputText.value;
+    fetch(appendInputTextToUrl(inputText)).then(Response => Response.json()).then(json => console.log(json));
 })
+
+
+
